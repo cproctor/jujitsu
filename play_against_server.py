4a44ca4b0dbf1f2c5bbe8278cd1a6db312111892
Chris Proctor <chris.proctor@gmail.com>
2015-06-25 23:19:36 -0700

    Allow the player to use her own name

--- play_against_server.py
+++ play_against_server.py
@@ -2,9 +2,11 @@ from jujitsu_library import jujitsu, human_player, create_remote_player
 
 # Connects to a jujitsu server running on this computer. If you know the IP 
 # address and port for a different computer running a jujitsu server, 
-# you can connect to that server by entering its address below.
-remote = create_remote_player("http://localhost:5555/jujitsu", 5555)
+# you can connect to that server by entering its address instead of 'localhost'
+# below.
+remote = create_remote_player("http://localhost", 5555)
 
 # You could replace the human player with your own chooser function here
 # to test your program against the server.
-jujitsu(human_player, remote, "Player", "The computer")
+human_name = raw_input("What is your name? ")
+jujitsu(human_player, remote, human_name, "The computer")
