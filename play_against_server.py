d37b30336c903e57afd112dd97e3f3961cbc1421
Chris Proctor <chris.proctor@gmail.com>
2015-06-25 23:21:21 -0700

    Improved instructions

--- play_against_server.py
+++ play_against_server.py
@@ -1,9 +1,10 @@
 from jujitsu_library import jujitsu, human_player, create_remote_player
 
-# Connects to a jujitsu server running on this computer. If you know the IP 
-# address and port for a different computer running a jujitsu server, 
-# you can connect to that server by entering its address instead of 'localhost'
-# below.
+# Connects to a jujitsu server running on this computer. (Make sure you 
+# run start_server.py in another Terminal window before running this
+# script.) If you know the IP address and port for a different computer 
+# running a jujitsu server, you can connect to that server by entering its 
+# address instead of 'localhost' below.
 remote = create_remote_player("http://localhost", 5555)
 
 # You could replace the human player with your own chooser function here
